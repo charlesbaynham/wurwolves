642f60dc47ce21bf65e9d67c53994224b54f6207
Charles Baynham <charles.baynham@npl.co.uk>
2020-06-01 00:37:00 +0100

    Use demo data

--- frontend/src/demoData.js
+++ frontend/src/demoData.js
@@ -40,14 +40,34 @@ export function addDemoData(dispatch) {
     }
 
     dispatch(addPlayer({
-        id: "123",
+        id: "0",
         name: "Hello world",
         status: "normal"
     }))
     dispatch(addPlayer({
-        id: "321",
-        name: "Next one",
+        id: "1",
+        name: "Dead one",
         status: "wolfed"
+    }))
+    dispatch(addPlayer({
+        id: "2",
+        name: "Lynched one",
+        status: "lynched"
+    }))
+    dispatch(addPlayer({
+        id: "3",
+        name: "Nommed one",
+        status: "nominated"
+    }))
+    dispatch(addPlayer({
+        id: "4",
+        name: "Seconded one",
+        status: "seconded"
+    }))
+    dispatch(addPlayer({
+        id: "5",
+        name: "Spectator",
+        status: "spectating"
     }))
 
     return (
