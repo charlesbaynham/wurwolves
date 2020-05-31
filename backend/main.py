c06722cab12d2b783c7bb6bacba9c6ddfcb5a943
Charles Baynham <charles.baynham@npl.co.uk>
2020-05-31 17:26:37 +0100

    Inject uuid

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,29 @@
+from datetime import datetime
 from uuid import uuid4 as get_uuid
 
-from fastapi import Cookie, FastAPI, Response
+from fastapi import Cookie, Depends, FastAPI, Response
 
 app = FastAPI()
 
+data = {}
+
 
 @app.get("/items/")
-async def read_items(*, response: Response, fakesession: str = Cookie(None)):
-    if fakesession is None:
+async def user_id(*, response: Response, session_UUID: str = Cookie(None)):
+    if session_UUID is None:
         new_val = get_uuid()
-        fakesession = new_val
-        response.set_cookie(key="fakesession", value=new_val)
-    return {"fakesession": fakesession}
+        session_UUID = new_val
+        response.set_cookie(key="session_UUID", value=new_val)
+    return session_UUID
+
+
+@app.get("/stuff/")
+async def read_stuff(*, user_ID=Depends(user_id)):
+    return str(user_ID)
+
+
+@app.get("/log_connection/")
+async def log_connection(*, user_ID=Depends(user_id)):
+    time_str = datetime.now().isoformat()
+    data[user_ID] = time_str
+    return data
