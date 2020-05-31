dae19d76b9e9f3a258aa0cd2019a90452e640721
Charles Baynham <charles.baynham@npl.co.uk>
2020-05-31 19:23:52 +0100

    Break out Player

--- frontend/src/features/player/Player.js
+++ frontend/src/features/player/Player.js
@@ -1,12 +1,41 @@
 import React, { useState } from 'react';
 
 
-function Player() {
-    const [playerStatus, setPlayerStatus] = useState('spectator');
+var IMAGE_LOOKUP = {
+    'normal': {
+        'img': '/images/person.svg',
+        'alt': 'A normal villager?'
+    },
+    'lynched': {
+        'img': '/images/person-lynched.svg',
+        'alt': 'A lynched player'
+    },
+    'nominated': {
+        'img': '/images/person-nominated.svg',
+        'alt': 'A nominated player'
+    },
+    'wolfed': {
+        'img': '/images/person-wolfed.svg',
+        'alt': 'A player killed by a wolf'
+    },
+    'seconded': {
+        'img': '/images/person-seconded.svg',
+        'alt': 'A seconded player'
+    },
+    'seconded': {
+        'img': '/images/person.svg',
+        'alt': 'A spectator'
+    }
+}
+
+function Player(props) {
+    const [playerStatus, setPlayerStatus] = useState('normal');
 
     return (
         <figure class="col-4 col-sm-3 figure player">
-            <img src="/images/person.svg" class="figure-img img-fluid w-100" alt="A normal villager" />
+            <img src={IMAGE_LOOKUP[playerStatus].img}
+                class="figure-img img-fluid w-100"
+                alt={IMAGE_LOOKUP[playerStatus].alt} />
             <figcaption class="figure-caption text-center">Charles</figcaption>
         </figure>
     )
