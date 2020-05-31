84b2fe3a8d76befe9aa279a3fc599beea1b91b3e
Charles Baynham <charles.baynham@npl.co.uk>
2020-05-31 22:21:37 +0100

    Write players slice

--- frontend/src/features/stateSlices/players.js
+++ frontend/src/features/stateSlices/players.js
@@ -12,7 +12,7 @@ class PlayerData {
 export const playersSlice = createSlice({
   name: 'players',
   initialState: {
-    value: [],
+    value: {},
   },
   reducers: {
     addPlayer: (state, action) => {
@@ -20,15 +20,37 @@ export const playersSlice = createSlice({
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      new_player = PlayerData(action.payload.id, action.payload.name, action.payload.status, false)
-      state.push(new_player)
-      return state
+      new_player = PlayerData(action.payload.id, action.payload.name, action.payload.status, false);
+      state[action.payload.id] = new_player;
+      return state;
     },
-    decrement: state => {
-      state.value -= 1;
+    removePlayer: (state, action) => {
+      idToRemove = action.payload;
+      delete state[idToRemove];
+      return state;
     },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    setPlayerName: (state, action) => {
+      state[action.payload.id].name = action.payload.name
+      return state;
+    },
+    setPlayerStatus: (state, action) => {
+      state[action.payload.id].status = action.payload.status
+      return state;
+    },
+    setPlayerSelected: (state, action) => {
+      state[action.payload.id].selected = action.payload.selected
+      return state;
     },
   },
 });
+
+export const { addPlayer, removePlayer, setPlayerName, setPlayerStatus, setPlayerSelected } = counterSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state) => state.counter.value)`
+export const getPlayerStatus = id => state => state.players[id].status;
+export const getPlayerName = id => state => state.players[id].name;
+export const getPlayerSelected = id => state => state.players[id].selected;
+
+export default playersSlice.reducer;
