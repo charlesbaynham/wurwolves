68aa228d5efc616ff6abae9fdf962a2880671f7f
Charles Baynham <charles.baynham@npl.co.uk>
2020-06-03 21:25:45 +0100

    Setup production server with /api proxy

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 3000;
+
+// proxy middleware options
+const options = {
+  target: 'http://localhost:8000', // target host
+  changeOrigin: true, // needed for virtual hosted sites
+  ws: true, // proxy websockets
+  pathRewrite: {
+    // '^/api/old-path': '/api/new-path', // rewrite path
+    '^/api': '/', // remove base path
+  },
+  // router: {
+  //   // when request.headers.host == 'dev.localhost:3000',
+  //   // override target 'http://www.example.org' to 'http://localhost:8000'
+  //   'dev.localhost:3000': 'http://localhost:8000',
+  // },
+};
+
+// create the proxy (without context)
+const apiProxy = createProxyMiddleware(options);
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
@@ -25,11 +45,8 @@ if (!isDev && cluster.isMaster) {
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-  // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
-  });
+  // mount `apiProxy` in web server
+  app.use('/api', apiProxy);
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
