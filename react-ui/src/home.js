b153e0de1034dd5e58fa9f0a5f2267979a20890f
Charles Baynham <charles.baynham@npl.co.uk>
2020-06-05 23:07:03 +0100

    First frontend to backend comms! Implement new game button

--- react-ui/src/home.js
+++ react-ui/src/home.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
 
 
-function Home() {
-    return (
-        <div className="container limited-width pt-5 bg-light bg-night-dark">
-            Welcome to Wurwolves...
-        </div>
-    );
+class Home extends Component {
+    state = {
+        url: ""
+    }
+
+    render() {
+        return (
+            <div className="container limited-width pt-5 bg-light bg-night-dark">
+                Welcome to Wurwolves...
+
+                <Button href={this.state.url}>
+                    Start a new game
+                </Button>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        fetch('/api/get_game')
+            .then(res => res.json())
+            .then((data) => {
+                var newUrl = '/' + data;
+                this.setState({ url: newUrl })
+            })
+            .catch(console.log)
+    }
 }
 
 export default Home;
