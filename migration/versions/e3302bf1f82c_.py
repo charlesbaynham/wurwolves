915bcf46e792b458378cf149e370d81371b4782e
Charles Baynham <charles.baynham@npl.co.uk>
2020-06-05 17:54:00 +0100

    Migration

---
+++ migration/versions/e3302bf1f82c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e3302bf1f82c
+Revises: 23376ece95d8
+Create Date: 2020-06-05 17:53:50.675857
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e3302bf1f82c'
+down_revision = '23376ece95d8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
