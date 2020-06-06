0974556e320fd1a2662f3e8a3877d20ba1f1da5e
Charles Baynham <charles.baynham@npl.co.uk>
2020-06-06 13:56:26 +0100

    Write fixtures for db testing

---
+++ tests/test_database.py
@@ -0,0 +1,39 @@
+import os
+
+import pytest
+
+from backend.model import GameEvent, GameEventVisibility
+
+
+TESTING_DB_URL = "sqlite:///testing.db"
+
+
+@pytest.fixture(scope="session")
+def engine():
+    os.environ['DATABASE_URL'] = TESTING_DB_URL
+
+    from backend.database import engine
+
+    return engine
+
+
+@pytest.fixture
+def session(engine):
+    from backend.model import Base
+    from backend.database import Session
+
+    Base.metadata.bind = engine
+
+    Base.metadata.drop_all()
+    Base.metadata.create_all()
+
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
+
+
+def test_fixtures(session):
+    assert len(session.query(GameEvent).all()) == 0
+    assert len(session.query(GameEventVisibility).all()) == 0
