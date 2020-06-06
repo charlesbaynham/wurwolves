f0e3984f901f7d997ba242c0f867c9ad908c1c06
Charles Baynham <charles.baynham@npl.co.uk>
2020-06-06 01:22:22 +0100

    Dumb database messing around working

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from fastapi import Depends, FastAPI, Path
 
 from .database import session_scope
 from .user_id import user_id
-from .model import GameEvent
+from .model import GameEvent, EventType
 
 
 WORDS_FILE = os.path.join(os.path.dirname(__file__), 'words.txt')
@@ -22,13 +22,17 @@ async def ui_events(
         user_ID=Depends(user_id)
 ):
     with session_scope() as session:
-        events = session.query(GameEvent).all()
+        events = session.query(GameEvent.game_id, GameEvent.event_type).all()
         return events
 
 
 @app.get("/api/stuff/")
 async def read_stuff(*, user_ID=Depends(user_id)):
-    return "Hello " + str(user_ID)
+    with session_scope() as session:
+        session.add(GameEvent(
+            game_id=123, event_type=EventType.GUI,
+            details={"user_id": user_ID}, public_visibility=True
+        ))
 
 
 @app.get("/api/log_connection/")
