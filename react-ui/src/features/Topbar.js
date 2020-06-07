3364e6286fee52a343634570242898742bd76319
Charles Baynham <charles.baynham@npl.co.uk>
2020-06-07 18:37:55 +0100

    Add text box

--- react-ui/src/features/Topbar.js
+++ react-ui/src/features/Topbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
 
 
 function Topbar() {
     return (
       <Navbar expand="lg" bg="light" className="bg-secondary bg-night-black">
         <a className="navbar-brand" href="/">Wurwolves</a>
+        <Form className="ml-auto">
+          <Form.Control placeholder="Your name..." />
+        </Form>
       </Navbar>
     )
   }
