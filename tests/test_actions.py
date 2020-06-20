0997346b7de41e7f2da1025b101d3954c9be3c4f
Charles Baynham <charles.baynham@gmail.com>
2020-06-20 13:36:32 +0100

    More tests passing

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,9 +1,10 @@
 from uuid import uuid4 as uuid
 
 import pytest
+from fastapi import HTTPException
 
 from backend.game import WurwolvesGame
-from backend.model import GameStage
+from backend.model import PlayerRole
 
 GAME_ID = "hot-potato"
 USER_ID = uuid()
@@ -24,3 +25,40 @@ def demo_game(db_session) -> WurwolvesGame:
 def test_spectator_lobby(demo_game):
     assert hasattr(demo_game, "spectator_lobby_action")
     demo_game.spectator_lobby_action(USER_ID)
+
+
+def test_wolf_night(demo_game):
+    demo_game.start_game()
+
+    players = demo_game.get_players_model()
+
+    assert hasattr(demo_game, "wolf_night_action")
+
+    for player in players:
+        if player.role == PlayerRole.WOLF:
+            demo_game.wolf_night_action(player.user_id)
+
+
+def test_medic_night(demo_game):
+    demo_game.start_game()
+
+    players = demo_game.get_players_model()
+
+    assert hasattr(demo_game, "medic_night_action")
+
+    for player in players:
+        if player.role == PlayerRole.MEDIC:
+            demo_game.medic_night_action(player.user_id)
+
+
+def test_medic_forbidden_wolf(demo_game):
+    demo_game.start_game()
+
+    players = demo_game.get_players_model()
+
+    assert hasattr(demo_game, "medic_night_action")
+
+    for player in players:
+        if player.role == PlayerRole.MEDIC:
+            with pytest.raises(HTTPException):
+                demo_game.wolf_night_action(player.user_id)
