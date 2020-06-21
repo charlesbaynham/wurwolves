1998e0ce49cc833fc4e55c56fbd3c9d5d053f31e
Charles Baynham <charles.baynham@gmail.com>
2020-06-21 23:44:39 +0100

    Resize bike

--- react-ui/src/features/Footer.js
+++ react-ui/src/features/Footer.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className="text-muted footer d-flex flex-row-reverse px-5 bg-light bg-night-black">
+        <div className="text-muted footer d-flex flex-row justify-content-end px-5 bg-light bg-night-black">
+            <div id="bike" className="d-flex flex-column justify-content-center"><img src="images/bike.png" /></div>
             <p>Baynham Design</p>
         </div>
     )
