c5576d0697450eaddf640362fb5890dea673fb4a
Charles Baynham <charles.baynham@gmail.com>
2020-06-27 23:07:11 +0100

    Scroll chatbox on end.
    Closes #18

--- react-ui/src/features/Chatbox.js
+++ react-ui/src/features/Chatbox.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Form from 'react-bootstrap/Form';
+import ScrollableFeed from 'react-scrollable-feed'
 
 import {
     selectMessages, selectShowSecretChat
 } from './selectors'
 
+
 function ChatEntry(props) {
     return (<p>{props.msg}</p>)
 }
+
 
 export function Chatbox(props) {
 
@@ -46,12 +49,12 @@ export function Chatbox(props) {
         <div className="col-md-5">
             <div className="card card-body d-flex flex-column chat-holder bg-night-black">
                 <h5 className="card-title">Events</h5>
-                <div id="chat-box" className="flex-grow-1">
+                <ScrollableFeed className="chat-box flex-grow-1">
                     {chat_messages.map((m, ind) => m.isStrong
                         ? <strong key={ind}><ChatEntry msg={m.msg} /></strong>
                         : <ChatEntry key={ind} msg={m.msg} />
                     )}
-                </div>
+                </ScrollableFeed>
                 {secretChat}
             </div>
         </div>
