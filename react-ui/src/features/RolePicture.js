a53c1c5843cbb62ef1acf6f95088b8cde873fe20
Charles Baynham <charles.baynham@gmail.com>
2020-07-19 23:02:13 +0100

    Use seed (not yet random)

--- react-ui/src/features/RolePicture.js
+++ react-ui/src/features/RolePicture.js
@@ -20,13 +20,13 @@ const num_pics = {
 export function RolePicture(props) {
 
     if (num_pics[props.role] > 0) {
-        const image_details = {
-            'img': `/images/characters/${props.role.toLowerCase()}0.svg`,
-            'alt': 'Picture of your role'
-        }
+
+        const pic_num = Math.floor(props.seed * num_pics[props.role])
+
+        const pic_url = `/images/characters/${props.role.toLowerCase()}${pic_num}.svg`
 
         return (
-            <img className="role_picture" src={image_details.img} alt={image_details.alt} />
+            <img className="role_picture" src={pic_url} alt={`Picture of a ${props.role}`} />
         )
     } else {
         return null
