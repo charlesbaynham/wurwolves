0e2c8fabe0d417b3dd9a58a1951a64cb2893d8eb
Charles Baynham <charles.baynham@gmail.com>
2020-07-20 22:17:36 +0100

    Give masons chat

--- backend/roles/mason.py
+++ backend/roles/mason.py
@@ -30,6 +30,7 @@ description = RoleDescription(
         GameStage.DAY: StageAction(text=general_desc),
     },
     team=Team.VILLAGERS,
+    secret_chat_enabled=True,
     fallback_role=PlayerRole.VILLAGER,
     fallback_role_description=villager,
 )
