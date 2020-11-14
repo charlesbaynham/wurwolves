c38450c66ed9ae9f8b1bc95f0fac091bdcf5bda4
Charles Baynham <bayners123@gmail.com>
2020-11-14 10:00:09 +0000

    Announce acolyte (#53)
    Closes #47

--- backend/roles/acolyte.py
+++ backend/roles/acolyte.py
@@ -4,6 +4,8 @@ The Acolyte role
 The acolyte has no special powers but wins if the wolves win. The wolves know who they are, but the acolyte
 doesn't know anything. For the sake of the game's conclusion, the Acolyte counts as a villager.
 """
+from typing import TYPE_CHECKING
+
 from ..model import GameStage
 from ..model import PlayerRole
 from .common import RoleDescription
@@ -11,6 +13,9 @@ from .common import RoleDetails
 from .common import StageAction
 from .teams import Team
 from .villager import description as villager
+
+if TYPE_CHECKING:
+    from ..game import WurwolvesGame
 
 general_desc = """
 You really like the idea of the werewolves, but you don't know who they are. Still, do your best to help
@@ -34,7 +39,35 @@ description = RoleDescription(
 )
 
 
+def announce_to_wolves(game: "WurwolvesGame"):
+    acolytes = game.get_players_model(role=PlayerRole.ACOLYTE)
+    wolves = game.get_players_model(role=PlayerRole.WOLF)
+
+    if acolytes:
+        if len(acolytes) == 1:
+            msg = "There's an acolyte in the game: it's {}".format(
+                acolytes[0].user.name
+            )
+
+        else:
+            msg = "There are acolytes in the game: they are {}".format(
+                " & ".join(a.user.name for a in acolytes)
+            )
+
+        game.send_chat_message(
+            msg,
+            is_strong=True,
+            player_list=[p.id for p in wolves],
+        )
+
+
 def register(role_map):
     role_map.update(
-        {PlayerRole.ACOLYTE: RoleDetails(role_description=description, actions={})}
+        {
+            PlayerRole.ACOLYTE: RoleDetails(
+                role_description=description,
+                actions={},
+                startup_callback=announce_to_wolves,
+            )
+        }
     )
