8850c10b593459b3796e34a9d068423b7be2298e
Charles Baynham <charles.baynham@gmail.com>
2020-11-21 13:04:59 +0000

    Add a beer donation link

--- react-ui/src/features/Footer.js
+++ react-ui/src/features/Footer.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className="text-muted footer d-flex flex-row justify-content-end px-5 bg-light bg-night-black">
+        <div className="text-muted text-nowrap footer d-flex flex-row px-5 bg-light bg-night-black">
+            <a id="buymeabeer" href="https://www.buymeacoffee.com/wurwolves">
+                Buy me a beer
+                <span role="img" aria-label="beer">🍺</span>
+            </a>
+            <span className="flex-grow-1"></span>
             <div id="bike" className="d-flex flex-column justify-content-center"><img src="images/bike.png" alt="Baynham design motorbike logo" /></div>
             <p>Baynham Design</p>
-        </div>
+        </div >
     )
 }
 
