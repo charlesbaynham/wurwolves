bff60bd58bdad915bc140bb4ae93e0dae84457ce
Charles Baynham <charles.baynham@gmail.com>
2021-01-16 13:04:31 +0000

    Simple backend test

--- tests/test_hosted_backend.py
+++ tests/test_hosted_backend.py
@@ -9,17 +9,6 @@ API_URL = "http://localhost:8000/api/"
 
 
 def test_hello(backend_server):
-
-    response = requests.get(API_URL + "hello")
-
-    print(response.status_code)
-    print(response.content)
-
-    assert response.ok
-
-
-def test_hello2(backend_server):
-
     response = requests.get(API_URL + "hello")
 
     print(response.status_code)
