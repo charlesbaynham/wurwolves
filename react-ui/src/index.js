0fe7e9642d6916d20e1911ba47a66bfe61150db1
Charles Baynham <c.baynham@imperial.ac.uk>
2024-07-07 21:17:04 +0100

    New root API

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "./index.css";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root"),
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
